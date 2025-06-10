9fa81c1112e736e6637829227854b01ac6551366
Максим <@KOMPUTER>
2025-06-11 02:20:19 +0300

    Добавьте файлы проекта.

---
+++ avtoplanirovshik_start-exit/Form1.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace avtoplanirovshik_start_exit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.clock = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.clock_zapuska = new System.Windows.Forms.Label();
+            this.clock_dalit = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.shapka = new System.Windows.Forms.Panel();
+            this.minim = new System.Windows.Forms.Button();
+            this.exit = new System.Windows.Forms.Button();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.proc_del_button = new System.Windows.Forms.Button();
+            this.proc_del = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.shapka.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // clock
+            // 
+            this.clock.AutoSize = true;
+            this.clock.Location = new System.Drawing.Point(119, 47);
+            this.clock.Name = "clock";
+            this.clock.Size = new System.Drawing.Size(35, 13);
+            this.clock.TabIndex = 0;
+            this.clock.Text = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            // 
+            // clock_zapuska
+            // 
+            this.clock_zapuska.AutoSize = true;
+            this.clock_zapuska.Location = new System.Drawing.Point(13, 96);
+            this.clock_zapuska.Name = "clock_zapuska";
+            this.clock_zapuska.Size = new System.Drawing.Size(0, 13);
+            this.clock_zapuska.TabIndex = 1;
+            // 
+            // clock_dalit
+            // 
+            this.clock_dalit.AutoSize = true;
+            this.clock_dalit.Location = new System.Drawing.Point(13, 150);
+            this.clock_dalit.Name = "clock_dalit";
+            this.clock_dalit.Size = new System.Drawing.Size(35, 13);
+            this.clock_dalit.TabIndex = 2;
+            this.clock_dalit.Text = "label2";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // shapka
+            // 
+            this.shapka.BackColor = System.Drawing.Color.Silver;
+            this.shapka.Controls.Add(this.minim);
+            this.shapka.Controls.Add(this.exit);
+            this.shapka.Location = new System.Drawing.Point(0, 0);
+            this.shapka.Name = "shapka";
+            this.shapka.Size = new System.Drawing.Size(362, 25);
+            this.shapka.TabIndex = 5;
+            // 
+            // minim
+            // 
+            this.minim.BackColor = System.Drawing.Color.Silver;
+            this.minim.FlatAppearance.BorderSize = 0;
+            this.minim.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.minim.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.minim.Location = new System.Drawing.Point(312, 0);
+            this.minim.Name = "minim";
+            this.minim.Size = new System.Drawing.Size(25, 25);
+            this.minim.TabIndex = 1;
+            this.minim.Text = "_";
+            this.minim.UseVisualStyleBackColor = false;
+            this.minim.Click += new System.EventHandler(this.minim_Click);
+            this.minim.MouseEnter += new System.EventHandler(this.minim_MouseEnter);
+            this.minim.MouseLeave += new System.EventHandler(this.minim_MouseLeave);
+            // 
+            // exit
+            // 
+            this.exit.BackColor = System.Drawing.Color.Silver;
+            this.exit.FlatAppearance.BorderSize = 0;
+            this.exit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exit.Location = new System.Drawing.Point(337, 0);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(25, 25);
+            this.exit.TabIndex = 0;
+            this.exit.Text = "X";
+            this.exit.UseVisualStyleBackColor = false;
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            this.exit.MouseEnter += new System.EventHandler(this.exit_MouseEnter);
+            this.exit.MouseLeave += new System.EventHandler(this.exit_MouseLeave);
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(250, 89);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBox1.TabIndex = 6;
+            // 
+            // maskedTextBox2
+            // 
+            this.maskedTextBox2.Location = new System.Drawing.Point(250, 147);
+            this.maskedTextBox2.Name = "maskedTextBox2";
+            this.maskedTextBox2.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBox2.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(16, 274);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(493, 75);
+            this.button2.TabIndex = 9;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(16, 204);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(94, 17);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "Понедельник";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(16, 241);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(69, 17);
+            this.checkBox5.TabIndex = 11;
+            this.checkBox5.Text = "Пятница";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(404, 204);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(68, 17);
+            this.checkBox4.TabIndex = 12;
+            this.checkBox4.Text = "Четверг";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(280, 204);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(57, 17);
+            this.checkBox3.TabIndex = 13;
+            this.checkBox3.Text = "Среда";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(176, 204);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(68, 17);
+            this.checkBox2.TabIndex = 14;
+            this.checkBox2.Text = "Вторник";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.AutoSize = true;
+            this.checkBox7.Location = new System.Drawing.Point(219, 241);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Size = new System.Drawing.Size(93, 17);
+            this.checkBox7.TabIndex = 15;
+            this.checkBox7.Text = "Воскресенье";
+            this.checkBox7.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(116, 241);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(67, 17);
+            this.checkBox6.TabIndex = 16;
+            this.checkBox6.Text = "Суббота";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // proc_del_button
+            // 
+            this.proc_del_button.Location = new System.Drawing.Point(157, 474);
+            this.proc_del_button.Name = "proc_del_button";
+            this.proc_del_button.Size = new System.Drawing.Size(205, 52);
+            this.proc_del_button.TabIndex = 17;
+            this.proc_del_button.Text = "Сохранить";
+            this.proc_del_button.UseVisualStyleBackColor = true;
+            this.proc_del_button.Click += new System.EventHandler(this.proc_del_button_Click);
+            // 
+            // proc_del
+            // 
+            this.proc_del.Location = new System.Drawing.Point(16, 427);
+            this.proc_del.Multiline = true;
+            this.proc_del.Name = "proc_del";
+            this.proc_del.Size = new System.Drawing.Size(409, 41);
+            this.proc_del.TabIndex = 18;
+            this.proc_del.TextChanged += new System.EventHandler(this.proc_del_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 367);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(523, 544);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.proc_del);
+            this.Controls.Add(this.proc_del_button);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.checkBox7);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.maskedTextBox2);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.shapka);
+            this.Controls.Add(this.clock_dalit);
+            this.Controls.Add(this.clock_zapuska);
+            this.Controls.Add(this.clock);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.shapka.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label clock;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label clock_zapuska;
+        private System.Windows.Forms.Label clock_dalit;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Panel shapka;
+        private System.Windows.Forms.Button exit;
+        private System.Windows.Forms.Button minim;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.Button proc_del_button;
+        private System.Windows.Forms.TextBox proc_del;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ avtoplanirovshik_start-exit/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace avtoplanirovshik_start_exit
+{
+    public partial class Form1 : Form
+    {
+        bool drag = false;
+        Point start_point = new Point(0, 0);
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            // проверяем наше окно, и если оно было свернуто, делаем событие        
+            if (WindowState == FormWindowState.Minimized)
+            {
+                // прячем наше окно из панели
+                ShowInTaskbar = false;
+                // делаем нашу иконку в трее активной
+                //notifyIcon1.Visible = true;
+            }
+        }
+
+        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                // возвращаем отображение окна в панели
+                ShowInTaskbar = true;
+                //разворачиваем окно
+                WindowState = FormWindowState.Normal;
+            }
+            Activate();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button2.Text = "Выбор программы на \nавтовключения";
+            button2.Font = new Font("Microsoft Sans Serif", 14);
+            checkBox1.Font = new Font("Microsoft Sans Serif", 14);
+            checkBox2.Font = new Font("Microsoft Sans Serif", 14);
+            checkBox3.Font = new Font("Microsoft Sans Serif", 14);
+            checkBox4.Font = new Font("Microsoft Sans Serif", 14);
+            checkBox5.Font = new Font("Microsoft Sans Serif", 14);
+            checkBox6.Font = new Font("Microsoft Sans Serif", 14);
+            checkBox7.Font = new Font("Microsoft Sans Serif", 14);
+            proc_del.Font = new Font("Microsoft Sans Serif", 14);
+            proc_del_button.Font = new Font("Microsoft Sans Serif", 14);
+            timer1.Tick += new EventHandler(timer1_Tick);
+            // делаем нашу иконку в трее
+            notifyIcon1.Visible = true;
+            // добавляем Эвент или событие по 2му клику мышки, 
+            //вызывая функцию  notifyIcon1_MouseClick
+            notifyIcon1.MouseClick += new MouseEventHandler(notifyIcon1_MouseClick);
+            notifyIcon1.Icon = new Icon("icon.ico");
+            // добавляем событие на изменение окна
+            Resize += new EventHandler(Form1_Resize);
+            shapka.Size = new Size(Width, 25);
+            shapka.BackColor = Color.White;
+            shapka.MouseDown += new MouseEventHandler(shapka_MouseDown);
+            shapka.MouseMove += new MouseEventHandler(shapka_MouseMove);
+            shapka.MouseUp += new MouseEventHandler(shapka_MouseUp);
+            clock.Font = new Font("Microsoft Sans Serif", 16);
+            clock.Location = new Point(Width/2-clock.Right/2,50);
+            clock_zapuska.Font = new Font("Microsoft Sans Serif", 14);
+            clock_dalit.Font = new Font("Microsoft Sans Serif", 14);
+            clock_zapuska.Text = "Время запуска\nприложения";
+            clock_dalit.Text = "Время вырубания\nприложения";
+            maskedTextBox1.Mask = "00:00:00";
+            maskedTextBox2.Mask = "00:00:00";
+            button2.Click += button2_Click;
+            label1.Font = new Font("Microsoft Sans Serif", 14);
+            label1.Text = "Напишите процесс который нужно завершить\nпо истечении времени";
+            exit.Location = new Point(Width-25, 0);
+            minim.Location = new Point(Width-50, 0);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DateTime now = DateTime.Now;
+            //Console.WriteLine($"D: {now.ToString("D")}"); //D: 6 января 2022 г.
+            //Console.WriteLine($"d: {now.ToString("d")}"); //d: 06.01.2022
+            //Console.WriteLine($"F: {now.ToString("F")}"); //F: 6 января 2022 г. 14:45:20
+            //Console.WriteLine($"f: {now:f}"); //f: 6 января 2022 г. 14:45
+            //Console.WriteLine($"G: {now:G}"); //G: 06.01.2022 14:45:20
+            //Console.WriteLine($"g: {now:g}"); //g: 06.01.2022 14:45
+            //Console.WriteLine($"M: {now:M}"); //M: 6 января
+            //Console.WriteLine($"O: {now:O}"); //O: 2022-01-06T14:45:20.3942344+04:00
+            //Console.WriteLine($"o: {now:o}"); //o: 2022-01-06T14:45:20.3942344+04:00
+            //Console.WriteLine($"R: {now:R}"); //R: Thu, 06 Jan 2022 14:45:20 GMT
+            //Console.WriteLine($"s: {now:s}"); //s: 2022-01-06T14:45:20
+            clock.Text = $"T: {now:T} {now:dddd}"; //T: 14:45:20
+            //Console.WriteLine($"t: {now:t}"); //t: 14:45
+            //Console.WriteLine($"U: {now:U}"); //U: 6 января 2022 г. 10:45:20
+            //Console.WriteLine($"u: {now:u}"); //u: 2022-01-06 14:45:20
+            //Console.WriteLine($"Y: {now:Y}"); //Y: январь 2022 г.
+            if ($"{now:T}" == maskedTextBox1.Text.Replace(" ", ""))
+                if (check($"{now:dddd}"))
+                    start_exit_proc("avtosapusk.txt", true);
+            if ($"{now:T}" == maskedTextBox2.Text.Replace(" ", ""))
+                if (check($"{now:dddd}"))
+                    start_exit_proc("exit.txt", false);
+        }
+
+        private void start_exit_proc(string str, bool prov)
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(str, Encoding.GetEncoding("UTF-8")))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        if (prov)
+                            Process.Start(line);
+                        if (!prov)
+                        {
+                            var pr = Process.GetProcessesByName(line);
+                            foreach (Process process in pr)
+                                process.Kill();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка при запуске приложения: {ex.Message}");
+            }
+        }
+
+        bool check(string text)
+        {
+            Dictionary<string, CheckBox> days = new Dictionary<string, CheckBox>
+            {
+                ["понедельник"] = checkBox1,
+                ["вторник"] = checkBox2,
+                ["среда"] = checkBox3,
+                ["четверг"] = checkBox4,
+                ["пятница"] = checkBox5,
+                ["суббота"] = checkBox6,
+                ["воскресенье"] = checkBox7
+            };
+            var pr = days[text];
+            if (pr.Checked)
+                return true;
+            else 
+                return false;
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void minim_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void exit_MouseEnter(object sender, EventArgs e)
+        {
+            exit.BackColor = Color.Red;
+        }
+
+        private void exit_MouseLeave(object sender, EventArgs e)
+        {
+            exit.BackColor = Color.Silver;
+        }
+
+        private void minim_MouseEnter(object sender, EventArgs e)
+        {
+            minim.BackColor = Color.RoyalBlue;
+        }
+
+        private void minim_MouseLeave(object sender, EventArgs e)
+        {
+            minim.BackColor = Color.Silver;
+        }
+
+        private void shapka_MouseDown(object sender, MouseEventArgs e)
+        {
+            drag = true;
+            start_point = new Point(e.X, e.Y);
+        }
+
+        private void shapka_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (drag)
+            {
+                Point p = PointToScreen(e.Location);
+                Location = new Point(p.X - start_point.X, p.Y - start_point.Y);
+            }
+        }
+
+        private void shapka_MouseUp(object sender, MouseEventArgs e)
+        {
+            drag = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "All files (*.*)|*.*"; //  Укажите типы файлов, которые нужно показывать в окне выбора
+            openFileDialog.Title = "Выберите файл";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+                using (StreamWriter stream = new StreamWriter("avtosapusk.txt", true))
+                    stream.WriteLine(filePath);
+            }
+        }
+
+        private void proc_del_button_Click(object sender, EventArgs e)
+        {
+            using (StreamWriter stream = new StreamWriter("exit.txt", true))
+                stream.WriteLine(proc_del.Text);
+            proc_del.Text = "";
+        }
+
+        private void proc_del_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
